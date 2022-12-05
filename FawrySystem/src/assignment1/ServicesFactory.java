e83b7e46e96a60c3d99cb955b75437597c926c6b
dell <dell@192.168.1.4>
2022-12-05 02:53:02 +0200

    Service factory

---
+++ FawrySystem/src/assignment1/ServicesFactory.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+public class ServicesFactory {
+	
+	public static Services chooseServices(String Service) {
+		
+		if(Service == "mobile recharge")
+			return new MobileRecharge();
+		else if(Service == "internet payment")
+			return new InternetPayment();
+		else if(Service == "landline")
+			return new Landline();
+		else if(Service == "donation")
+			return new Donation();
+		
+		return null;
+	}
+
+}
