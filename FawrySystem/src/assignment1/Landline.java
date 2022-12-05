5feeed581abaede021b2cd78b44ef96796b35cc8
dell <dell@192.168.1.4>
2022-12-05 02:45:56 +0200

    update on landline Service class

--- FawrySystem/src/assignment1/Landline.java
+++ FawrySystem/src/assignment1/Landline.java
@@ -1,12 +1,49 @@
 package assignment1;
 
-public abstract class Landline implements Services {
+public class Landline extends Services {
 	
+	private String landline;
+	private  int amount;
+	private ReceiptServices receiptServices ;
+		
+	public String getLandline() {
+		return landline;
+	}
+
+	public void setLandline(String mobile) {
+		this.landline = mobile;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+
 	@Override
 	public void execute() {
-		System.out.println("Donations..");
+		System.out.println("landline is executed..");
+		
 	}
-	public abstract void getData(String place , int amount) ;
-	public abstract void pay(Payment method);
+
+
+	public Form createform() {
+		form = new LandlineForm();
+		return form ;
+	}
+
+	public void setReceipts() {
+		int receipt = ((LandlineForm)form).getChoice();
+		if(receipt == 1) {
+			this.receiptServices = new MonthlyReceipt();
+			this.receiptServices.execute();
+		}		
+		else if(receipt == 2)
+			this.receiptServices = new QuarterReceipt();
+	}
+
 
 }
