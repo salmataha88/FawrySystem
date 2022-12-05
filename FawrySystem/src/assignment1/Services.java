a384b499eb0e2748e5665fd3763692bd2f79fb4e
dell <dell@192.168.1.4>
2022-12-05 02:53:48 +0200

    update on services class

--- FawrySystem/src/assignment1/Services.java
+++ FawrySystem/src/assignment1/Services.java
@@ -1,9 +1,9 @@
 package assignment1;
 
-public interface Services {
-	
-	public  void execute();
-	public ServiceProviders chooseProviders(String provider);
-	public void setPayment(String method);
+public abstract class Services {
+
+	public Form form;	
+	public abstract void execute();
+	public abstract Form createform();
 	
 }
