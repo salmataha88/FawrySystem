06162d4fff71d195a0cf8833d2e0fd749660ae79
dell <dell@192.168.1.4>
2022-12-05 04:34:58 +0200

    update payment system

--- FawrySystem/src/assignment1/PaymentSystem.java
+++ FawrySystem/src/assignment1/PaymentSystem.java
@@ -9,11 +9,11 @@ public class PaymentSystem {
 	private static Scanner input = new Scanner(System.in);
 	
 	public void PayMethod(int amount) {
-		System.out.println("Enter your payment method: ");
+		System.out.println("Write your payment method: ");
 		System.out.println("1.credit card  2.cash  3.wallet");
 		method = input.nextLine();
 		this.payment = PaymentFactory.choosePayment(method);
-		System.out.println("Enter your  method: "+ method);
+		
 		if(method.equals("credit card") ) {
 			System.out.println("Enter your card number: ");
 			String cardNumber = input.nextLine();
@@ -22,13 +22,14 @@ public class PaymentSystem {
 			payment = new CreditCard(date , cardNumber);
 			payment.pay(amount);
 		}
-		else if(method == "cash") {
-			
+		else if(method.equals("cash")) {
+			payment = new Cash();
+			payment.pay(amount);
 		}
-		else if(method == "wallet") {
-			
+		else if(method.equals("wallet")) {
+			payment = new Wallet();
+			payment.pay(amount); 
 		}
-		else System.out.println("Enter your  method: "+ method);
 	}
 
 }
